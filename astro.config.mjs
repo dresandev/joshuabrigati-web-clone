5850a3a197e508b44bdb6cb0ef8ae146c43cfbbe
Dresan <dresan.dev@gmail.com>
2024-09-14 20:35:39 -0500

    add: github actions flow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://dresan.dev',
-});
+  site: 'https://joshuabrigati-web-clone.github.io',
+})
